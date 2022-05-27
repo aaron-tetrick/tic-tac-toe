bcf311bec90732ad58427ea4fafb4674a45099f9
Aaron Tetrick <atetrick14102@gmail.com>
2022-05-27 02:02:57 -0400

    Add layout and stylings for webpage

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-console.log("Tic Tac Toe");
+
+
+//GAMEBOARD MODULE
+const gameBoard = (() => {
+    const gameBoardArray = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+    
+    
+    return{gameBoardArray};
+
+})();
+
+
+
+let test = gameBoard.gameBoardArray;
+console.log(test);
+
+//PLAYER FACTORY FUNCTION
+const Players = (name, mark) => {
+        const getName = () => name;
+        const getMark = () => mark;
+ 
+ 
+    return{getName, getMark};
+};
+
+
+const player1 = Players('Player 1', 'X');
+const player2 = Players('Player 2', 'O');
+
+
+
+console.log(player1.getName());
+console.log(player2.getName());
