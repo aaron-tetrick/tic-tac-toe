2641b57effc2c430d62ad4c5cfb7bd7952eeb720
Aaron Tetrick <atetrick14102@gmail.com>
2022-05-29 03:27:50 -0400

    Made responsive gameBoard array

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const Players = (name, mark) => {
     const getName = () => name;
     const getMark = () => mark;
 
-
 return{getName, getMark};
 };
 
@@ -13,31 +12,18 @@ const player2 = Players('Player 2', 'O');
 
 //GAMEBOARD MODULE
 const gameBoard = (() => {
-  const t1 = document.querySelector('.t1')
-  const t2 = document.querySelector('.t2')
-  const t3 = document.querySelector('.t3')
-  const m1 = document.querySelector('.m1')
-  const m2 = document.querySelector('.m2')
-  const m3 = document.querySelector('.m3')
-  const b1 = document.querySelector('.b1')
-  const b2 = document.querySelector('.b2')
-  const b3 = document.querySelector('.b3')
-
-    const gameBoardArray = [t1.innerText, t2.innerText, t3.innerText, m1.innerText, m2.innerText, m3.innerText, b1.innerText, b2.innerText, b3.innerText];
+    const gameBoardArray = [];
     return{gameBoardArray};
 
 })();
 
-
-
 //DISPLAY CONTROLLER
-const displayController = ((player) => {
+const displayController = (() => {  
+    const cells = document.querySelectorAll('.cell')
     const p1 = document.querySelector('.p1');
     const p2 = document.querySelector('.p2');
-    const cells = document.querySelectorAll('.cell')
-    const cellsArray = Array.from(cells);
-    const h1 = document.querySelector('h1');
-
+   
+    //Runs placeMarker function on each cell
     cells.forEach(cell => {
         cell.addEventListener('click', placeMarker)
     });
@@ -47,38 +33,68 @@ const displayController = ((player) => {
         containerArray = Array.from(container);
         let X = 0;
         let O = 0;
+
         //Finds out whose turn it is
         containerArray.forEach(cell => {
             if (cell.innerText === 'X') {
                 X++;
-                console.log(X);
             } else if (cell.innerText === 'O') {
                 O++;
-                console.log(O);
             }
         });
+
+        //Places marker and indicates the next player's turn (Try making this a finction and putting it outside the placeMarker function???)
         if (e.target.innerText === '') {
             if (X <= O) {
                 e.target.innerText = player1.getMark();
                 p1.classList.remove('players-turn');
                 p2.classList.add('players-turn');
-        } else if (X > O) {
+                p2.style.transition = "1s";
+                p1.style.transition = "0.5s";
+            }   else if (X > O) {
                 e.target.innerText = player2.getMark();
                 p2.classList.remove('players-turn');
                 p1.classList.add('players-turn');
-        }
-    }   
+                p1.style.transition = "0.5s";
+                p2.style.transition = "1s";
+            }
 
+            searchGameBoard();
+        } 
+        
+        console.log(gameBoard.gameBoardArray);
+
+        //Update gameBoardArray
+        gameBoard.gameBoardArray = [t1.innerText, t2.innerText, t3.innerText,
+                                    m1.innerText, m2.innerText, m3.innerText,
+                                    b1.innerText, b2.innerText, b3.innerText];
+        return gameBoard.gameBoardArray;
     }
 
-    
+    console.log(gameBoard.gameBoardArray);
+    function searchGameBoard() {
+    gameBoard.gameBoardArray.forEach(marker => {
+            console.log(marker);
+    });
+};
 
-return{h1};
+
+    
+    //Restart Game
+    const restart = document.querySelector('.restart')
+    restart.addEventListener('click', restartGame);
+
+    function restartGame() {
+        cells.forEach(cell => {
+            cell.innerText = '';
+        });
+        p2.classList.remove('players-turn');
+        p1.classList.add('players-turn');
+        gameBoard.gameBoardArray = [];
+        console.log(gameBoard.gameBoardArray);
+    }
+return{};
 
 })();
-
-
-//An if statement that says if there are more O's
-
 
 
